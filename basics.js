4b1115107dfa3b7da6b310245698986d0ab88b6a
Rajveersinghchouhan21883 <rajveersingh21883@email.com>
2026-06-26 16:08:17 +0530

    javascript files

---
+++ basics.js
@@ -0,0 +1,21 @@
+const accountId = "123234345"
+let accountName ="rajveer"
+var state = "rajasthan"
+accountNo = "123"
+let stateDate 
+
+
+/*
+mainly we not use the var in the js 
+because it will give the issue in the block scope {} and function scope fun{}
+mainly it will can all the same var name data if we can at a sigle place also 
+*/
+
+// accountId = "raj234"   //this thing is not possible in const because we can declared it only once 
+accountName = "raj"
+state = "madhya pradesh"
+accountNo ="321"
+
+console.log(accountId)
+
+console.table([accountId, accountName,state,accountNo,stateDate])
