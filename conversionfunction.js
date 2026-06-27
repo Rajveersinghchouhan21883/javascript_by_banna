dced28c54407b85d75ad58160bfe48683ad851b4
Rajveersinghchouhan21883 <rajveersingh21883@email.com>
2026-06-27 18:51:34 +0530

    conversion and funcrion

---
+++ conversionfunction.js
@@ -0,0 +1,38 @@
+let score ="raj "
+console.log(typeof score)
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+/*
+null=> object,number,0
+undefined=>undefined,number,NaN
+" "=> string,number,0
+"raj"=>string,number,NaN
+*/
+
+
+
+
+
+let loggedIn =0
+
+let booleanLoggedIn = Boolean(loggedIn)
+//console.log(booleanLoggedIn)
+/* 
+1=>true
+0=>false
+" "=>false
+"raj"=>true
+*/
+let someNumber =33
+
+let stringNumber =String(someNumber)
+//console.log(stringNumber)
+//33 =>33
+
+
+let no =100
+ ++no  + no++
+console.log(no)
