4ea07d9072afa5617aabce850418921b59f4bf6c
Rajveersinghchouhan21883 <rajveersingh21883@email.com>
2026-06-27 23:19:25 +0530

    strings operations

---
+++ string.js
@@ -0,0 +1,18 @@
+const name ="rajveer"
+const count =40
+
+//console.log(name +count);
+//console.log(`name ${name} and count ${count}`);
+//console.log(` ${name}${count}`);
+
+const gameName = new String("rajveer- rj -com")
+
+/*console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(4))
+console.log(gameName.indexOf('j'))
+console.log(gameName.substring(0,6)) */
+console.log(gameName.slice(-16,10))
+
+
+
