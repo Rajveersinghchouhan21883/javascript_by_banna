15b42787f4efba796ef344278d4c3485f115f7cc
Rajveersinghchouhan21883 <rajveersingh21883@email.com>
2026-06-28 19:59:38 +0530

    Number and Math

---
+++ numberandmaths.js
@@ -0,0 +1,41 @@
+/*
+let score = 400
+console.log(score)
+
+let balance =new Number(100)
+console.log(balance)
+
+console.log(balance.toString());
+console.log(typeof balance.toString());
+
+console.log(balance.toFixed(2))
+
+let otherNumber = 34.8966
+console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(2));
+console.log(otherNumber.toPrecision(1));
+
+const hundreds = 10000000
+console.log(hundreds.toLocaleString('en-IN'));
+
+*/
+
+/********maths****** */
+
+/*console.log(Math);
+console.log(Math.abs(-5));
+console.log(Math.round(8.6));
+console.log(Math.ceil(5.5));
+console.log(Math.floor(5.5));
+console.log(Math.max(3,4,5,3,2));
+*/
+//console.log(Math.random());
+//console.log(Math.random()*10 +1);
+//console.log(Math.floor(Math.random()*10 +1));
+
+
+const min =20
+const max=30
+
+console.log(Math.floor(Math.random() *(max - min +1)) +min);
+
